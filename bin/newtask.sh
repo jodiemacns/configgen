9f4e57006f96305d20c80dbd09c8e19cc089b28c
Jodie MacEachern <jodiemacns@gmail.com>
2020-02-14 09:27:40 -0400

    Modified template.

--- bin/newtask.sh
+++ bin/newtask.sh
@@ -1,7 +1,8 @@
 LOWER=$(echo $1 | tr '[:upper:]' '[:lower:]')
 UPPER=$(echo $1 | tr '[:lower:]' '[:upper:]')
-
+mkdir -p out
 echo It is $UPPER  ... $LOWER
-sed s/COMMS/$UPPER/g task.c.template | sed s/comms/$LOWER/g | sed  s/__USER/$USER/g | sed s/__DATE/"$(date)"/g >$LOWER.c
-sed s/COMMS/$UPPER/g task.h.template | sed s/comms/$LOWER/g | sed  s/__USER/$USER/g | sed s/__DATE/"$(date)"/g >$LOWER.h
+sed s/COMMS/$UPPER/g ./template/task.c.template  | sed s/comms/$LOWER/g | sed  s/__USER/$USER/g | sed s/__DATE/"$(date)"/g >./out/$LOWER.c
+sed s/COMMS/$UPPER/g ./template/task.h.template  | sed s/comms/$LOWER/g | sed  s/__USER/$USER/g | sed s/__DATE/"$(date)"/g >./out/$LOWER.h
+sed s/COMMS/$UPPER/g ./template/task_noos.c.template | sed s/comms/$LOWER/g | sed  s/__USER/$USER/g | sed s/__DATE/"$(date)"/g >./out/$LOWER_noos.c
 #sed s/comms/abc/g comms.c | sed s/COMMS/ABC/g
