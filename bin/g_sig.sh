9bf6c9a3e5cd89170b10ba5bcd1c8dc0c831e57b
Jodie MacEachern <jodiemacns@gmail.com>
2020-09-18 23:51:54 -0300

    finished I thinkg on g_sig.sh

--- bin/g_sig.sh
+++ bin/g_sig.sh
@@ -6,29 +6,31 @@ ACTION=$1
 FILE=$2
 USER=$2
 
-
 #--------------------------------------------------------------------------------
 # Help menu 
 if [ "$ACTION" == "?" ]
 then
-    echo "*** ? = help.              $0 ?           "
-    echo "*** s = Sign Document      $0 s doc.pdf   "
-    echo "*** v = verify Document    $0 v           "
-    echo "*** i = import key         $0 i           "
-    echo "*** C = Create signing key $0 C           "
-    echo "*** E = export key         $0 E           "
-    echo "*** z = zip Document       $0 z           "
-    echo "*** u = unzip Document     $0 u           "
-    echo "*** e = encrypt file       $0 e           "
-    echo "*** e = decrypt file       $0 e           "
+    echo "*** ? = help.              g_sig.sh ?           "
+    echo "*** S = Sign Document      g_sig.sh S doc.pdf   "
+    echo "*** v = verify Document    g_sig.sh v           "
+    echo "*** i = import key         g_sig.sh i           "
+    echo "*** E = export key         g_sig.sh E           "
+    echo "*** C = Create signing key g_sig.sh C           "
+    echo "*** z = zip Document       g_sig.sh z           "
+    echo "*** u = unzip Document     g_sig.sh u           "
+    echo "*** e = encrypt file       g_sig.sh e           "
+    echo "*** d = decrypt file       g_sig.sh e           "
     exit
 fi
 
 #--------------------------------------------------------------------------------
 # Sign file
-if [ "$1" == "s" ]
+if [ "$1" == "S" ]
 then
-    gpg -a --output $FILE.sig  --detach-sig $FILE
+    gpg -a --output $FILE.tmp.sig  --detach-sig $FILE
+    cat $FILE.tmp.sig >> $FILE.sig
+    chmod 777 $FILE.sig
+    rm $FILE.tmp.sig
     exit
 fi
 
@@ -36,7 +38,7 @@ fi
 # Verify file
 if [ "$1" == "v" ]
 then
-    gpg --verify $FILE.sig  $FILE
+    gpg --status-fd 1 --verify $FILE.sig  $FILE 2>/dev/null | grep GOODSIG
     exit
 fi
 
@@ -57,6 +59,22 @@ then
 fi
 
 #--------------------------------------------------------------------------------
+# Fingerprint key
+if [ "$1" == "f" ]
+then
+    gpg --fingerprint $USER
+    exit
+fi
+
+#--------------------------------------------------------------------------------
+# Create key
+if [ "$1" == "C" ]
+then
+    gpg --full-generate-key
+    exit
+fi
+
+#--------------------------------------------------------------------------------
 # Compress file
 if [ "$1" == "z" ]
 then
@@ -72,3 +90,29 @@ then
     exit
 fi
 
+#--------------------------------------------------------------------------------
+# Encrypt file
+if [ "$1" == "e" ]
+then
+    command="gpg --output $FILE.gpg --encrypt "
+    shift 
+    shift 
+    while (( "$#" )); do 
+        echo $1 
+        command+="--recipient $1 "
+        shift 
+    done
+    command+="$FILE"
+    $command
+    exit
+fi
+
+#--------------------------------------------------------------------------------
+# Decrypt file
+if [ "$1" == "d" ]
+then
+    command="gpg -d $FILE"
+    command=$(echo $command | sed 's/\.gpg//')
+    $command
+    exit
+fi
